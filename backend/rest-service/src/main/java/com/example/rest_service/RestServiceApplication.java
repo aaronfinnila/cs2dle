c2f89e0bf35475dc1b027420ba6b2e2e9b0c5195
Aaron <aaronfinnilaa@gmail.com>
2026-01-30 14:54:30 +0200

    dotenv stuff backend

--- backend/rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
+++ backend/rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
@@ -11,9 +11,11 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		if (dotenv.entries().isEmpty() == false) {
+			System.setProperty("DB_USER", dotenv.get("DB_USER"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		}
 
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
