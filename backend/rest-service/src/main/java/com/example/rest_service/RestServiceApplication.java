4a2fe9dd4f963e08407ebbf43ca7f2b71a7579bf
Aaron <aaronfinnilaa@gmail.com>
2026-01-30 14:40:45 +0200

    backend small tweaks

--- backend/rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
+++ backend/rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
@@ -9,7 +9,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
