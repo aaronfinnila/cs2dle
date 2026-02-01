3434e5ebe297de8f40435ce4077d9cdfa7b1d4af
Aaron <aaronfinnilaa@gmail.com>
2026-02-01 22:26:22 +0200

    changed cloudflare env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
   const rollPlayerId = () => Math.floor(Math.random() * 110)
   const [correctPlayerId] = useState(rollPlayerId)
 
-  // api url
+  // api url fix
   const baseUrl = import.meta.env.VITE_API_URL;
 
   const handleCloseAbout = () => {
